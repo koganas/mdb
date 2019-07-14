1d98aa503c6e3a4b08e100e3c33bc5a1eaa64cb0
Koganas <danielsank@gmail.com>
2019-07-14 14:39:37 -0300

    change click event behavior

--- app/src/components/movieCard.js
+++ app/src/components/movieCard.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react'
-import MovieModal from './movieModal'
 
 class MovieLink extends Component {
 
-    state = {
-    	isSelected: false
-    }
-
-	toggle(e) {
-        e.preventDefault()
-        //e.target.parentNode.parentNode.querySelectorAll('.modal')
-
-        console.log(this.props.movie.id)
-        this.setState({ isSelected: !this.state.isSelected })
-	}
-
     render() {
         return (
-            <li className="list__item" onClick={this.toggle.bind(this)} >
+            <li className="list__item" onClick={this.props.onClick} >
             	<img src={'http://image.tmdb.org/t/p/w200' + this.props.movie.poster_path} alt="" />
                 <h1>{this.props.movie.title}</h1>
-                <MovieModal
-                	movie={this.props.movie}
-                	display={this.state.isSelected}
-                />
             </li>
         )
     }
--- app/src/components/movieSearch.js
+++ app/src/components/movieSearch.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import MovieCard from './movieCard'
+import MovieModal from './movieModal'
 
 class MovieList extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            search: ''
+            search: '',
+            isSelected: false,
+            modalMovie: ''
         }
     }
 
@@ -16,9 +19,31 @@ class MovieList extends Component {
         })
     }
 
+    closeModal(e) {
+        //e.target.parentNode.parentNode.querySelectorAll('.modal')
+        // e.target.parentNode.parentNode.querySelectorAll('.modal--open').forEach((elm)=> {
+        //     elm.classList.remove('modal--open')
+        // })
+
+        this.setState({
+            isSelected: !this.state.isSelected
+        })
+    }
+
+    toggleModal(e, index) {
+        e.preventDefault()
+        
+        this.setState({
+            isSelected: true,
+            modalMovie: this.props.movies[index]
+        })
+
+    }
+
     render() {
         return (
             <div className="container">
+
                 <input type="text"
                     placeholder="Search"
                     value={this.state.search}
@@ -26,19 +51,26 @@ class MovieList extends Component {
                 />
 
                 {this.props.movies ? (
-                    <ul className="list">
-                        {
-                            this.props.movies.filter(movie =>
-                                movie.title.toLowerCase().includes(this.state.search.toLowerCase())
-                            ).map(
-                                (movie, index) => { return <MovieCard movie={movie} key={index} /> }
-                            )
-                        }
-                    </ul>
+                    <div>
+                        <ul className="list">
+                            {
+                                this.props.movies.filter(movie =>
+                                    movie.title.toLowerCase().includes(this.state.search.toLowerCase())
+                                ).map(
+                                    (movie, index) => { return <MovieCard movie={movie} key={index} onClick={(e)=>this.toggleModal(e, index)} /> }
+                                )
+                            }
+                        </ul>
+                        <MovieModal
+                            movie={this.state.modalMovie}
+                            display={this.state.isSelected}
+                        />
+                    </div>
                 ) :
                     (
                     <p>Try searching for a movie</p>
                 )}
+
             </div>
         )
 
