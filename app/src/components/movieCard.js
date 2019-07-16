7e2140cb3072f42b78e572827a4b5bd8817d5dbb
Koganas <danielsank@gmail.com>
2019-07-15 22:11:28 -0300

    typography; mobile res adjustments; format modal;

--- app/src/components/movieCard.js
+++ app/src/components/movieCard.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 
 class MovieLink extends Component {
-
     render() {
         const { movie, onClick } = this.props
         return (
             <li className="list__item" onClick={onClick} >
                 {movie.poster_path ?
                     (
-            	       <img src={'http://image.tmdb.org/t/p/w200' + movie.poster_path} alt="" />
+            	       <img src={'http://image.tmdb.org/t/p/w185' + movie.poster_path} alt="" />
                     ) : (
-                       <img src="https://via.placeholder.com/200x300.jpg" alt="" />
+                       <img src="https://via.placeholder.com/185x278.jpg" alt="" />
                     )
                 }
                 <h1 className="list__title">{movie.title}</h1>
--- app/src/components/movieModal.js
+++ app/src/components/movieModal.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 class MovieModal extends Component {
 
     render() {
         const { movie, onClick, display } = this.props
-        let release_date = moment(movie.release_date).format('MMMM DD')
+        let release_date = moment(movie.release_date).format('MMMM DD, YYYY')
         let genres = movie.genre_ids ? movie.genre_ids.map((genre, i) => <li key={i}>{genre}</li> ) : ''
 
         return (
@@ -13,19 +15,21 @@ class MovieModal extends Component {
                 <div className="modal__container">
                     {movie.poster_path ?
                         (
-                	       <img src={'http://image.tmdb.org/t/p/w342' + movie.poster_path} alt="" width="342" height="513" />
+                	       <img src={'http://image.tmdb.org/t/p/w342' + movie.poster_path} alt="" />
                         ) : (
                            <img src="https://via.placeholder.com/342x513.jpg" alt="" />
                         )
                     }
                     <h1>{movie.title}</h1>
+                    <h2>Release date: {release_date}</h2>
+                    <ul>{genres}</ul>
                     <p>{movie.overview}</p>
 
-                    <ul>{genres}</ul>
-                    
-                    {release_date}
-
-                    <button onClick={onClick}>Close</button>
+                    <FontAwesomeIcon
+                        icon={faTimes}
+                        onClick={onClick}
+                        className="modal__close"
+                    />
                 </div>
             </div>
         )
